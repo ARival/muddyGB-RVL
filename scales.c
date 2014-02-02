3a83030b7bc7517ad53160cbc9c291893dbaac3c
Jack Willis <jack@attac.us>
2014-02-02 15:30:54 -0600

    Add more comments

--- scales.c
+++ scales.c
@@ -11,7 +11,10 @@ SCALE   ionian[] = { C, D,  E,  F,  G,  A,  B, C2 };
 SCALE  aeolian[] = { C, D, Ds,  F,  G, Gs, As, C2 };
 
 SCALE harmonic[] = { C, D, Ds,  F,  G, Gs,  B, C2 };
-SCALE    blues[] = { C, D, Ds,  F, Fs, Gs, As, C2 };
 
 SCALE   dorian[] = { C, D, Ds,  F,  G,  A, As, C2 };
 SCALE   lydian[] = { C, D,  E, Fs,  G,  A,  B, C2 };
+
+/* The 'blue' scale uses quarter-tones. (see music.c.)
+ * It is equivalent to C, D, Ed, F, G, A, Bd, C2.
+ */
